e2ed169cc2b938e1caeebe5591334bca5c7d370f
Sharon Lin <hsiang.lin@gmail.com>
2020-07-19 22:34:25 -0400

    Add xtra credit, pass all four tests, had to put all variables within the function to make the tests pass for some reason, don't know why

--- grades.js
+++ grades.js
@@ -1,12 +1,14 @@
-let mockAssignmentAverages = []
-let priceIsRightTotal = 0
-let tvTotalScore = 0
-let orderbookTotal = 0
-let tddTotal = 0
-let fantasyTotal = 0
+
 
 
 function calculateAverageByAssignment(mockGrades) {
+  let mockAssignmentAverages = []
+  let priceIsRightTotal = 0
+  let tvTotalScore = 0
+  let orderbookTotal = 0
+  let tddTotal = 0
+  let fantasyTotal = 0
+
   if (!mockGrades.length) {
     return mockAssignmentAverages
   }
@@ -41,33 +43,64 @@ function calculateAverageByAssignment(mockGrades) {
   }
 }
 
-module.exports = calculateAverageByAssignment
+const mockGrades2 = [
+  { assignment: 'The Price is Right', student: 'Shondra', score: 85 },
+  { assignment: 'TV Synopsis', student: 'Evelyn', score: 88 },
+  { assignment: 'Order Book', student: 'Steve', score: 100 },
+  { assignment: 'TDD Strict Equality', student: 'Jamal', score: 98 },
+  { assignment: 'The Price is Right', student: 'Evelyn', score: 89 },
+  { assignment: 'Fantasy Scoring', student: 'Jamal', score: 95 },
+  { assignment: 'TDD Strict Equality', student: 'Steve', score: 87 },
+  { assignment: 'TV Synopsis', student: 'Steve', score: 88 },
+  { assignment: 'Fantasy Scoring', student: 'Shondra', score: 96 },
+  { assignment: 'TDD Strict Equality', student: 'Evelyn', score: 96 },
+  { assignment: 'Fantasy Scoring', student: 'Evelyn', score: 98 },
+  { assignment: 'Order Book', student: 'Shondra', score: 78 },
+  { assignment: 'The Price is Right', student: 'Jamal', score: 96 },
+  { assignment: 'Order Book', student: 'Jamal', score: 100 },
+  { assignment: 'The Price is Right', student: 'Steve', score: 90 },
+  { assignment: 'TV Synopsis', student: 'Shondra', score: 100 },
+  { assignment: 'TDD Strict Equality', student: 'Shondra', score: 92 },
+  { assignment: 'Order Book', student: 'Evelyn', score: 100 },
+  { assignment: 'Fantasy Scoring', student: 'Steve', score: 93 },
+  { assignment: 'TV Synopsis', student: 'Jamal', score: 98 },
+]
 
-// const mockGrades2 = [
-//   { assignment: 'The Price is Right', student: 'Shondra', score: 85 },
-//   { assignment: 'TV Synopsis', student: 'Evelyn', score: 88 },
-//   { assignment: 'Order Book', student: 'Steve', score: 100 },
-//   { assignment: 'TDD Strict Equality', student: 'Jamal', score: 98 },
-//   { assignment: 'The Price is Right', student: 'Evelyn', score: 89 },
-//   { assignment: 'Fantasy Scoring', student: 'Jamal', score: 95 },
-//   { assignment: 'TDD Strict Equality', student: 'Steve', score: 87 },
-//   { assignment: 'TV Synopsis', student: 'Steve', score: 88 },
-//   { assignment: 'Fantasy Scoring', student: 'Shondra', score: 96 },
-//   { assignment: 'TDD Strict Equality', student: 'Evelyn', score: 96 },
-//   { assignment: 'Fantasy Scoring', student: 'Evelyn', score: 98 },
-//   { assignment: 'Order Book', student: 'Shondra', score: 78 },
-//   { assignment: 'The Price is Right', student: 'Jamal', score: 96 },
-//   { assignment: 'Order Book', student: 'Jamal', score: 100 },
-//   { assignment: 'The Price is Right', student: 'Steve', score: 90 },
-//   { assignment: 'TV Synopsis', student: 'Shondra', score: 100 },
-//   { assignment: 'TDD Strict Equality', student: 'Shondra', score: 92 },
-//   { assignment: 'Order Book', student: 'Evelyn', score: 100 },
-//   { assignment: 'Fantasy Scoring', student: 'Steve', score: 93 },
-//   { assignment: 'TV Synopsis', student: 'Jamal', score: 98 },
-// ]
-
- //const emptyArray = []
+// const emptyArray = []
 
 // console.log(calculateAverageByAssignment([]))
 
- //console.log(calculateAverageByAssignment(mockGrades2))
+// console.log(calculateAverageByAssignment(mockGrades2))
+
+
+function calculateAverageByStudent(mockGrades) {
+  let shondraTotal = 0
+  let evelynTotal = 0
+  let steveTotal = 0
+  let jamalTotal = 0
+  let mockStudentAverages = []
+
+  function sumScore(acc, individual) {
+    return acc + individual.score
+  }
+
+  if (!mockGrades.length) {
+    return mockStudentAverages
+  }
+
+  shondraTotal = mockGrades.filter((individual) => individual.student === 'Shondra').reduce(sumScore, 0)
+  // .reduce((acc, individual.score)=> {accu + individual.score}, 0), tried put this into the reduce but didn't work
+  evelynTotal = mockGrades.filter((individual) => individual.student === 'Evelyn').reduce(sumScore, 0)
+  steveTotal = mockGrades.filter((individual) => individual.student === 'Steve').reduce(sumScore, 0)
+  jamalTotal = mockGrades.filter((individual) => individual.student === 'Jamal').reduce(sumScore, 0)
+
+  mockStudentAverages.push({ name: 'Shondra', average: shondraTotal / 5 }, { name: 'Evelyn', average: evelynTotal / 5 },
+    { name: 'Steve', average: steveTotal / 5 }, { name: 'Jamal', average: jamalTotal / 5 })
+
+  return mockStudentAverages
+}
+
+console.log(calculateAverageByStudent(mockGrades2))
+
+module.exports = { calculateAverageByAssignment, calculateAverageByStudent }
+// need the curly brackets because in the test file {object}, so multiple functions can be passed
